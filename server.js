6b214c42453e52069b8b1a02adbf9801d8626a0d
yueshi <yue.shi@caseware.com>
2020-12-29 15:38:02 -0500

    change run button to basic

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.static(__dirname + '/'));
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/realworld'));
+app.use(express.static('./dist/crossbrowsertesting'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/realworld/'}),
+  res.sendFile('index.html', {root: 'dist/crossbrowsertesting/'}),
 );
 
 // Start the app by listening on the default Heroku port
